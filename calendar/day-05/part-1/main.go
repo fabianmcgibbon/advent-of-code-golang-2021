c3715f987f58acc9245e3ad2b1455481a67ac35c
Fabian.McGibbon <fabian.mcgibbon@bjss.com>
2021-12-06 20:48:58 +0000

    day 5 - completed

---
+++ calendar/day-05/part-1/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"adventofcode/utils/files"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+const gridSize = 1000
+
+type coord struct {
+	x int
+	y int
+}
+
+type floor struct {
+	grid        [gridSize][gridSize]int
+	dangerAreas int
+}
+
+func main() {
+	input := files.ReadFile(5, "\n")
+	f := generateFloor(input)
+	fmt.Println(f.dangerAreas)
+}
+
+func generateFloor(input []string) floor {
+	var f floor
+
+	for _, entry := range input {
+		start, end := parseEntry(entry)
+		if start.x == end.x {
+			f.logY(start, end)
+		} else if start.y == end.y {
+			f.logX(start, end)
+		}
+	}
+
+	return f
+}
+
+func parseEntry(entry string) (start, end coord) {
+	coordinates := strings.Split(entry, " -> ")
+
+	s := strings.Split(coordinates[0], ",")
+	e := strings.Split(coordinates[1], ",")
+
+	start.x, _ = strconv.Atoi(s[0])
+	start.y, _ = strconv.Atoi(s[1])
+	end.x, _ = strconv.Atoi(e[0])
+	end.y, _ = strconv.Atoi(e[1])
+
+	fmt.Println(start)
+	fmt.Println(end)
+
+	return start, end
+}
+
+func (f *floor) logX(start, end coord) {
+	if start.x > end.x {
+		start, end = end, start
+	}
+	for x := start.x; x <= end.x; x++ {
+		f.grid[start.y][x]++
+		if f.grid[start.y][x] == 2 {
+			f.dangerAreas++
+		}
+	}
+}
+
+func (f *floor) logY(start, end coord) {
+	if start.y > end.y {
+		start, end = end, start
+	}
+	for y := start.y; y <= end.y; y++ {
+		f.grid[y][start.x]++
+		if f.grid[y][start.x] == 2 {
+			f.dangerAreas++
+		}
+	}
+}
